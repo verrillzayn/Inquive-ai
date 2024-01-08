3048293d40bc24df129351c111f7d1ee5199bbba
verzynx <verzynx@gmail.com>
2024-01-08 10:25:09 +0700

    chore: adding trpc provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "@/style/globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/marketing-page/navbar";
+import TrpcProvider from "@/components/providers/trpc-providers";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,15 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <body
-        className={cn(
-          "grainy min-h-screen bg-background font-sans antialiased ",
-          fontSans.variable,
-        )}
-      >
-        <Navbar />
-        {children}
-      </body>
+      <TrpcProvider>
+        <body
+          className={cn(
+            "grainy min-h-screen bg-background font-sans antialiased ",
+            fontSans.variable,
+          )}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </TrpcProvider>
     </html>
   );
 }
