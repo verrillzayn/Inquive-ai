f72bdfd1dda674e59c004d943f511271a2cdb372
verzynx <verzynx@gmail.com>
2024-01-08 23:36:51 +0700

    feat: setup dark mode

--- src/components/marketing-page/navbar.tsx
+++ src/components/marketing-page/navbar.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/max-widht-wrapper";
+import ThemeToogle from "@/components/theme-toggle";
 import { buttonVariants } from "@/components/ui/button";
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
@@ -6,9 +7,9 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-      <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+    <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-primary/5 bg-primary-foreground/75 backdrop-blur-lg transition-all dark:border-b-0">
+      <MaxWidthWrapper className="max-w-6xl">
+        <div className="flex h-14 items-center justify-between">
           <Link href="/" className="z-40 flex font-semibold">
             <span>Inquive</span>
           </Link>
@@ -17,6 +18,7 @@ const Navbar = () => {
 
           <div className="hidden items-center space-x-4 sm:flex">
             <>
+              <ThemeToogle />
               <Link
                 className={buttonVariants({
                   variant: "ghost",
