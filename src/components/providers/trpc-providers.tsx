93f9d5e7c3c6f2627c70cae947d2dfe6cf43ea99
verzynx <verzynx@gmail.com>
2024-01-08 10:29:03 +0700

    fix: typo

--- src/components/providers/trpc-providers.tsx
+++ src/components/providers/trpc-providers.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
 
-const TrpcProviider = ({ children }: PropsWithChildren) => {
+const TrpcProvider = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -24,4 +24,4 @@ const TrpcProviider = ({ children }: PropsWithChildren) => {
   );
 };
 
-export default TrpcProviider;
+export default TrpcProvider;
