d5b75240ca66c2a5e1996b95f257a869f2016286
verzynx <verzynx@gmail.com>
2024-01-08 14:45:58 +0700

    chore: adding documentId page

---
+++ src/app/(dashboard)/dashboard/[fieldId]/page.tsx
@@ -0,0 +1,49 @@
+import ChatWrapper from "@/components/dashboard-page/main-page/chat-wrapper";
+import PdfRenderer from "@/components/dashboard-page/main-page/pdf-renderer";
+
+import { db } from "@/db";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+interface PageProps {
+  params: {
+    fieldId: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { fieldId } = params;
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fieldId}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fieldId,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
+
+  return (
+    <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer />
+          </div>
+        </div>
+
+        <div className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
