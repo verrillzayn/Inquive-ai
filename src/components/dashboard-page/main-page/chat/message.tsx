ccc40399b97d913b71bc7cbe1de55f40630f9d05
verzynx <verzynx@gmail.com>
2024-01-10 04:25:33 +0700

    feat: displaying messages

---
+++ src/components/dashboard-page/main-page/chat/message.tsx
@@ -0,0 +1,78 @@
+import { Icons } from "@/components/inquive-icons";
+import { cn } from "@/lib/utils";
+import { ExtendedMessage } from "@/types/message";
+import { format } from "date-fns";
+import ReactMarkdown from "react-markdown";
+
+interface IMessageProps {
+  isNextMessagesIsSamePerson: boolean;
+  message: ExtendedMessage;
+}
+
+const Message = ({ isNextMessagesIsSamePerson, message }: IMessageProps) => {
+  return (
+    <div
+      className={cn("flex items-end", {
+        "justify-end": message.isUserMessages,
+      })}
+    >
+      <div // user and logo icon
+        className={cn("relative flex h-7 w-7 items-center justify-center", {
+          "order-2 rounded-full bg-primary/90 text-primary-foreground":
+            message.isUserMessages,
+          "order-1 rounded-full bg-primary p-0.5": !message.isUserMessages,
+          invisible: isNextMessagesIsSamePerson,
+        })}
+      >
+        {message.isUserMessages ? (
+          <Icons.User className="fill-primary/25 text-primary-foreground" />
+        ) : (
+          <Icons.logo className="h-3/4 w-3/4 fill-primary-foreground" />
+        )}
+      </div>
+
+      <div
+        className={cn("mx-2 flex max-w-md flex-col space-y-2 text-base", {
+          "border-1 items-end": message.isUserMessages,
+          "order-2 items-start": !message.isUserMessages,
+        })}
+      >
+        <div // chat bubble
+          className={cn("inline-block rounded-xl px-4 py-2", {
+            "bg-primary text-primary-foreground": message.isUserMessages,
+            "bg-primary/10 text-primary": !message.isUserMessages,
+            "rounded-br-none":
+              !isNextMessagesIsSamePerson && message.isUserMessages,
+            "rounded-bl-none":
+              !isNextMessagesIsSamePerson && !message.isUserMessages,
+          })}
+        >
+          {typeof message.text === "string" ? (
+            <ReactMarkdown
+              className={cn("prose", {
+                "text-primary-foreground/90": message.isUserMessages,
+                "text-primary/85": !message.isUserMessages,
+              })}
+            >
+              {message.text}
+            </ReactMarkdown>
+          ) : (
+            message.text
+          )}
+          {message.id !== "loading-message" ? (
+            <div
+              className={cn("mt-2 w-full select-none text-right text-xs", {
+                "text-primary": !message.isUserMessages,
+                "text-primary-foreground": message.isUserMessages,
+              })}
+            >
+              {format(new Date(message.createdAt), "HH:mm")}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- src/components/dashboard-page/main-page/chat/messages.tsx
+++ src/components/dashboard-page/main-page/chat/messages.tsx
@@ -1,7 +1,89 @@
-const Messages = () => {
+import Loader from "@/components/loader";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { trpc } from "@/app/_trpc/client";
+
+import { INFINITE_QUERY_LIMIT } from "@/config/inifinite-query";
+
+import { ChatBubbleIcon } from "@radix-ui/react-icons";
+import Message from "@/components/dashboard-page/main-page/chat/message";
+
+interface IMessagesProps {
+  fileId: string;
+}
+
+const Messages = ({ fileId }: IMessagesProps) => {
+  const { data, isLoading, fetchNextPage } =
+    trpc.getFileMessages.useInfiniteQuery(
+      {
+        fileId,
+        limit: INFINITE_QUERY_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        keepPreviousData: true,
+      },
+    );
+
+  const loadingMessages = {
+    createdAt: new Date().toISOString(),
+    id: "loading-messages",
+    isUserMessages: false,
+    text: (
+      <span className="flex h-full items-center justify-center">
+        <Loader className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  };
+
+  const messages = data?.pages.flatMap((page) => page.messages);
+
+  const combineMessages = [
+    ...(true ? [loadingMessages] : []),
+    ...(messages ?? []),
+  ];
+
   return (
-    <div>
-      <div>messages</div>
+    <div className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col-reverse gap-4 overflow-y-auto border-primary/20 p-3">
+      {combineMessages && combineMessages.length > 0 ? (
+        combineMessages.map((msg, index) => {
+          const isNextMessagesIsSamePerson =
+            combineMessages[index - 1]?.isUserMessages ===
+            combineMessages[index].isUserMessages;
+
+          if (index === combineMessages.length - 1) {
+            return (
+              <Message
+                key={msg.id}
+                isNextMessagesIsSamePerson={isNextMessagesIsSamePerson}
+                message={msg}
+              />
+            );
+          } else
+            return (
+              <Message
+                key={msg.id}
+                isNextMessagesIsSamePerson={isNextMessagesIsSamePerson}
+                message={msg}
+              />
+            );
+        })
+      ) : isLoading ? (
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+        </div>
+      ) : (
+        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+          <ChatBubbleIcon className="h-8 w-8 text-primary" />
+          <h3 className="text-xl font-semibold">You&apos;re all set!</h3>
+          <p className="text-sm text-primary/50">
+            Ask your first question to get started.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
