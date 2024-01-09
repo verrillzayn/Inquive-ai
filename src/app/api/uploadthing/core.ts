10f210b29037a64f23723c0860386d0c4cb4a38f
verzynx <verzynx@gmail.com>
2024-01-09 20:20:02 +0700

    feat: vectorize and store vecorize pdf to pinecone

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,13 @@
+import { pinecone } from "@/lib/pinecone";
+
 import { db } from "@/db";
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/community/vectorstores/pinecone";
 
 const f = createUploadthing();
 
@@ -32,6 +39,53 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
+
+      try {
+        // AI things
+        const res = await fetch(
+          file.url ||
+            `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+        );
+
+        const blob = await res.blob();
+
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmount = pageLevelDocs.length;
+
+        //vectorize and index entire PDF document / ubah tiap halaman pdf menjadi vector
+
+        const pineconeIndex = pinecone.Index("inquive");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+        console.log(error);
+      }
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
