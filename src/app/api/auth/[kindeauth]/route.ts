57abad0dcb8e9162f0f2e011c96182f5fc0463dd
verzynx <verzynx@gmail.com>
2024-01-11 02:09:58 +0700

    fix: passing endpoint

--- src/app/api/auth/[kindeauth]/route.ts
+++ src/app/api/auth/[kindeauth]/route.ts
@@ -1,3 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
 
-export const GET = handleAuth();
+export async function GET(req: NextRequest, { params }: any) {
+  const endpoint = params.kindeAuth;
+  return handleAuth(req, endpoint);
+}
