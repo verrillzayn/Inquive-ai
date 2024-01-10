0cf6e9f8f13b9f3c75db39a95fbf0ee804be167b
verzynx <verzynx@gmail.com>
2024-01-10 23:06:38 +0700

    chore: adding subscription plans configuration

---
+++ src/config/subscriptions-plans.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      ammount: 0,
+      priceIds: {
+        test: "",
+        productioin: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      ammount: 14,
+      priceIds: {
+        test: "someID",
+        productioin: "",
+      },
+    },
+  },
+];
