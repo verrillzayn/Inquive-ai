f93585466b49f9af5ffb2499dc26ba8445ef86bc
verzynx <verzynx@gmail.com>
2024-01-10 23:05:34 +0700

    fix: updating url

--- src/components/providers/trpc-providers.tsx
+++ src/components/providers/trpc-providers.tsx
@@ -11,7 +11,7 @@ const TrpcProvider = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/trpc`,
         }),
       ],
     }),
