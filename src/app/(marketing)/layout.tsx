3b5e964a1c92504341a025d4f2d17fc56d445e7e
verzynx <verzynx@gmail.com>
2024-01-10 22:59:34 +0700

    feat: adding footer

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/marketing-page/navbar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 
 const MarketingLayout = ({ children }: PropsWithChildren) => {
@@ -6,6 +8,33 @@ const MarketingLayout = ({ children }: PropsWithChildren) => {
     <>
       <Navbar />
       {children}
+      <div className="z-50 flex w-full items-center p-6">
+        <div className="hidden items-center gap-x-2 md:flex">
+          <Image
+            src="/logo.png"
+            height="40"
+            width="40"
+            alt="App Logo"
+            className="dark:hidden"
+          />
+          <Image
+            src="/logo-dark.png"
+            height="40"
+            width="40"
+            alt="App Logo"
+            className="hidden dark:block"
+          />
+          <p className="font-semibold">Inquive</p>
+        </div>
+        <div className="flex w-full items-center justify-between gap-x-2 text-muted-foreground md:ml-auto md:justify-end">
+          <Button variant="ghost" size="sm">
+            Privacy Policy
+          </Button>
+          <Button variant="ghost" size="sm">
+            Terms & Conditions
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
