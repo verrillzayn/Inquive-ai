cdb3adba6b65d8575e7bf1a9f32bb89f5c460ce7
verzynx <verzynx@gmail.com>
2024-01-10 23:56:42 +0700

    feat: addiing fake subscribe functioinality

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -8,88 +8,135 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { PineconeStore } from "@langchain/community/vectorstores/pinecone";
+import { PLANS } from "@/config/subscriptions-plans";
 
 const f = createUploadthing();
+
+const middleware = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) throw new Error("Unauthenticated");
+
+  const userDb = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+  // Whatever is returned here is accessible in onUploadComplete as `metadata`
+  return { userId: user.id, isSubscribe: userDb?.isSubscribe };
+};
+
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    key: string;
+    name: string;
+    url: string;
+  };
+}) => {
+  // This code RUNS ON YOUR SERVER after upload
+
+  const isFileExist = await db.file.findFirst({
+    where: {
+      key: file.key,
+    },
+  });
+
+  if (isFileExist) return;
+
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url:
+        file.url ||
+        `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: "PROCESSING",
+    },
+  });
+
+  try {
+    // AI things
+    const res = await fetch(
+      file.url ||
+        `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+    );
+
+    const blob = await res.blob();
+
+    const loader = new PDFLoader(blob);
+    const pageLevelDocs = await loader.load();
+
+    const pagesAmount = pageLevelDocs.length;
+    const { isSubscribe } = metadata;
+    const isProExceeded =
+      pagesAmount > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+    const isFreeExceeded =
+      pagesAmount > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+
+    if ((isSubscribe && isProExceeded) || (!isSubscribe && isFreeExceeded)) {
+      await db.file.update({
+        data: {
+          uploadStatus: "FAILED",
+        },
+        where: {
+          id: createdFile.id,
+        },
+      });
+    }
+
+    //vectorize and index entire PDF document / ubah tiap halaman pdf menjadi vector
+
+    const pineconeIndex = pinecone.Index("inquive");
+
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
+
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (error) {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+    console.log(error);
+  }
+
+  // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+  return { uploadedBy: metadata.userId };
+};
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      const { getUser } = getKindeServerSession();
-      const user = await getUser();
-
-      if (!user || !user.id) throw new Error("Unauthenticated");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-
-      const createdFile = await db.file.create({
-        data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url:
-            file.url ||
-            `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: "PROCESSING",
-        },
-      });
-
-      try {
-        // AI things
-        const res = await fetch(
-          file.url ||
-            `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-        );
-
-        const blob = await res.blob();
-
-        const loader = new PDFLoader(blob);
-        const pageLevelDocs = await loader.load();
-
-        const pagesAmount = pageLevelDocs.length;
-
-        //vectorize and index entire PDF document / ubah tiap halaman pdf menjadi vector
-
-        const pineconeIndex = pinecone.Index("inquive");
-
-        const embeddings = new OpenAIEmbeddings({
-          openAIApiKey: process.env.OPENAI_API_KEY,
-        });
-
-        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-          pineconeIndex,
-          namespace: createdFile.id,
-        });
-
-        await db.file.update({
-          data: {
-            uploadStatus: "SUCCESS",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      } catch (error) {
-        await db.file.update({
-          data: {
-            uploadStatus: "FAILED",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-        console.log(error);
-      }
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
