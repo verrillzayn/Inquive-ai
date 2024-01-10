2a25a1de9124e3f52eb3075b72aa7a1c2dc73d70
verzynx <verzynx@gmail.com>
2024-01-10 23:02:37 +0700

    style: change max-width

--- src/components/max-widht-wrapper.tsx
+++ src/components/max-widht-wrapper.tsx
@@ -9,7 +9,7 @@ const MaxWidthWrapper = ({
   children: ReactNode;
 }) => {
   return (
-    <div className={cn("mx-auto w-full max-w-2xl px-2.5 md:px-20", className)}>
+    <div className={cn("mx-auto w-full max-w-3xl px-2.5 md:px-20", className)}>
       {children}
     </div>
   );
