788afd58170f837b86a939d8586020e5b2d62688
verzynx <verzynx@gmail.com>
2024-01-10 23:00:35 +0700

    chore: fix classname, change tagline, adding image

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-background px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+      <MaxWidthWrapper className="mb-12 mt-20 flex flex-col items-center justify-center text-center sm:mt-6">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-muted bg-background px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 dark:shadow-zinc-500 dark:hover:border-gray-600">
           <p className="text-sm font-semibold text-foreground">
             Inquive is now public!
           </p>
@@ -21,8 +21,10 @@ export default function Home() {
           in seconds.
         </h1>
         <p className="mt-5 max-w-prose text-primary/70 sm:text-lg">
-          Inquive allows you to have conversation with any PDF documents. Simply
-          upload your file and start asking question right away.
+          Step into the world of Inquive, where PDFs come alive through the art
+          of conversation. Upload your documents, compose prompts, and let our
+          AI orchestrate a unique dialogue, creating a masterpiece of knowledge
+          and interaction.
         </p>
 
         <Button size="lg" className="mt-4" asChild>
@@ -33,51 +35,69 @@ export default function Home() {
       </MaxWidthWrapper>
       <div>
         <div className="relative isolate">
-          <div
-            aria-hidden={true}
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-35 sm:left-[calc(50%-30rem)] sm:w-[72.1875]"
-            />
-          </div>
-
           <div>
             <div className="lg;px-8 mx-auto max-w-6xl px-6">
               <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-xl bg-primary/5 p-2 ring-1 ring-inset ring-primary/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                  <Image
-                    src="/dashboard-preview.jpg"
-                    width={1364}
-                    height={866}
-                    alt="product preview"
-                    quality={100}
-                    className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8 md:p-20"
-                  />
+                <div className="-m-2 flex items-center justify-center rounded-xl p-2 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <div className="relative h-[300px]  w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
+                    <Image
+                      src="/documents.png"
+                      fill
+                      className="object-contain dark:hidden"
+                      alt="Documents Heroes"
+                    />
+                    <Image
+                      src="/documents-dark.png"
+                      fill
+                      className="hidden object-contain dark:block"
+                      alt="Documents Heroes"
+                    />
+                  </div>
+                  <div className="relative hidden h-[400px] w-[400px] md:block">
+                    <Image
+                      src="/reading.png"
+                      fill
+                      className="object-contain dark:hidden"
+                      alt="Reading Heroes"
+                    />
+                    <Image
+                      src="/reading-dark.png"
+                      fill
+                      className="hidden object-contain dark:block"
+                      alt="Reading Heroes"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div
-            aria-hidden={true}
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          >
-            <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-35 sm:left-[calc(50%-36rem)] sm:w-[72.1875]"
-            />
-          </div>
         </div>
       </div>
-
-      <div className="mx-auto my-32 max-w-5xl sm:mt-56">
+      <div
+        aria-hidden={true}
+        className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-35 sm:left-[calc(50%-30rem)] sm:w-[72.1875]"
+        />
+      </div>
+      <div className="relative mx-auto my-32 max-w-5xl sm:mt-56">
+        <div
+          aria-hidden={true}
+          className="pointer-events-none absolute inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-35 sm:left-[calc(50%-36rem)] sm:w-[72.1875]"
+          />
+        </div>
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="sm:texx-5xl mt-2 text-4xl font-bold text-primary/90">
@@ -133,21 +153,6 @@ export default function Home() {
             </div>
           </li>
         </ol>
-
-        <div className="lg;px-8 mx-auto max-w-6xl px-6">
-          <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-xl bg-primary/5 p-2 ring-1 ring-inset ring-primary/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <Image
-                src="/file-upload-preview.jpg"
-                width={1419}
-                height={732}
-                alt="file upload preview image"
-                quality={100}
-                className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8 md:p-20"
-              />
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
