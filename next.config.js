47b8864c892ed9f0bd23950382110bd7f95071b7
verzynx <verzynx@gmail.com>
2024-01-11 02:56:01 +0700

    fix: deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     config.resolve.alias.encoding = false;
     return config;
   },
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/dashboard`,
+  },
   async redirects() {
     return [
       {
