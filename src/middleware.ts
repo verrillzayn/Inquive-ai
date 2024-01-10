596ba56334b4ec22068360f4d541ca48b4026202
verzynx <verzynx@gmail.com>
2024-01-10 19:20:46 +0700

    chore: adding middleware

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  return withAuth(req);
+}
+export const config = {
+  matcher: ["/dashboard/:path*", "/auth-callback"],
+};
