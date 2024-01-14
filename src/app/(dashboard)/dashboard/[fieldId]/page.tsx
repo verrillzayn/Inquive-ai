d98ab1831cc08505d4f3f6d82f2cd728a538d43c
verzynx <verzynx@gmail.com>
2024-01-15 06:04:37 +0700

    fix: wrong flow

--- src/app/(dashboard)/dashboard/[fieldId]/page.tsx
+++ src/app/(dashboard)/dashboard/[fieldId]/page.tsx
@@ -1,14 +1,9 @@
 import ChatWrapper from "@/components/dashboard-page/main-page/chat/chat-wrapper";
 import PdfRenderer from "@/components/dashboard-page/main-page/pdf-renderer";
-import { PLANS } from "@/config/subscriptions-plans";
 
 import { db } from "@/db";
-import { pinecone } from "@/lib/pinecone";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { PineconeStore } from "@langchain/community/vectorstores/pinecone";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { notFound, redirect } from "next/navigation";
 
 interface PageProps {
@@ -25,90 +20,14 @@ const Page = async ({ params }: PageProps) => {
 
   if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fieldId}`);
 
-  const filePromise = db.file.findFirst({
+  const file = await db.file.findFirst({
     where: {
       id: fieldId,
       userId: user.id,
     },
   });
-  const dbUserPromise = db.user.findFirst({ where: { id: user.id } });
-
-  const [file, dbUser] = await Promise.all([filePromise, dbUserPromise]);
 
   if (!file) notFound();
-  if (!dbUser) throw new Error("not Authenticated");
-
-  try {
-    // AI things
-    const res = await fetch(
-      file.url ||
-        `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-    );
-
-    const blob = await res.blob();
-
-    const loader = new PDFLoader(blob);
-    const pageLevelDocs = await loader.load();
-
-    const pagesAmount = pageLevelDocs.length;
-    const { isSubscribe } = dbUser;
-    const isProExceeded =
-      pagesAmount > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
-    const isFreeExceeded =
-      pagesAmount > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
-
-    if ((isSubscribe && isProExceeded) || (!isSubscribe && isFreeExceeded)) {
-      await db.file.update({
-        data: {
-          uploadStatus: "FAILED",
-        },
-        where: {
-          id: file.id,
-        },
-      });
-    }
-
-    //vectorize and index entire PDF document / ubah tiap halaman pdf menjadi vector
-
-    const pineconeIndex = pinecone.Index("inquive");
-
-    const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY,
-    });
-
-    const pineconeStorePromise = PineconeStore.fromDocuments(
-      pageLevelDocs,
-      embeddings,
-      {
-        pineconeIndex,
-        namespace: file.id,
-      },
-    );
-
-    const dbUpdatePromise = db.file.update({
-      data: {
-        uploadStatus: "SUCCESS",
-      },
-      where: {
-        id: file.id,
-      },
-    });
-
-    const [pineconeStore, dbUpdate] = await Promise.all([
-      pineconeStorePromise,
-      dbUpdatePromise,
-    ]);
-  } catch (error) {
-    await db.file.update({
-      data: {
-        uploadStatus: "FAILED",
-      },
-      where: {
-        id: file.id,
-      },
-    });
-    console.log(error);
-  }
 
   return (
     <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
